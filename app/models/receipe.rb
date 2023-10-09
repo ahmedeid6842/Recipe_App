c06c9083bbe1de57d6abdc9bb57044f28e4521bf
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-10 01:22:53 +0300

    Created recipe model

---
+++ app/models/receipe.rb
@@ -0,0 +1,8 @@
+class Recipe < ApplicationRecord
+  belongs_to :user, foreign_key: "user_id"
+  has_many :recipe_foods, foreign_key: "recipe_id"
+  has_many :foods, through: :recipe_foods, foreign_key: "food_id"
+
+  validates :name, :preparation_time, :cooking_time, :description, presence: true
+  validates :public, inclusion: { in: [true, false] }
+end
