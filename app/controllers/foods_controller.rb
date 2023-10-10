915c6d7758e13882a7d9221e61cc02bd67cff1ce
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-10 08:46:09 +0300

    Fixed rubocop linters errors

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,16 +1,11 @@
 class FoodsController < ApplicationController
-  def show
-  end
+  def show; end
 
-  def index
-  end
+  def index; end
 
-  def new
-  end
+  def new; end
 
-  def create
-  end
+  def create; end
 
-  def destroy
-  end
+  def destroy; end
 end
