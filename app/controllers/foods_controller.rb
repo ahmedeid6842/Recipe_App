28c68319ef95fe53080d41778dd0a5cce9c4e80f
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-10 21:48:28 +0300

    Implement create food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -9,4 +9,20 @@ class FoodsController < ApplicationController
   def new
     @food = Food.new
   end
+
+  def create
+    @food = current_user.foods.new(food_params)
+
+    if @food.valid? && @food.save
+      flash[:success] = "Food has been added successfully"
+      redirect_to foods_path
+    else
+      Rails.logger.error("Failed to create food: #{@food.errors.full_messages}")
+      render :new
+    end
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
