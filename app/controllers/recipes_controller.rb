8c9af60427e7017982c7ca307ecc745f312005e0
Cesar Herrera <cesarherr321@gmail.com>
2023-10-10 16:40:32 -0300

    Add  new and create actions to the recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,36 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_user_by_id
+
   def show; end
 
-  def index; end
+  def index
+    @recipes = @user.recipes
+  end
 
-  def new; end
+  def new
+    @recipe = Recipe.new
+  end
 
-  def create; end
+  def create
+    @recipe = @user.recipes.new(recipe_params)
+    if @recipe.save
+      flash[:notice] = 'Recipe created successfully'
+      redirect_to recipes_path
+    else
+      flash[:alert] = 'Error! Recipe not created'
+      redirect_to new_recipe_path
+    end
+  end
 
-  def destroy; end
+  def find_user_by_id
+    @user = current_user
+  rescue ActiveRecord::RecordNotFound
+    flash[:error] = 'Error! User not found'
+    redirect_to users_url
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :cooking_time, :preparation_time, :public)
+  end
 end
