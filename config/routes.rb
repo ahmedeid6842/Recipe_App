bd9f8df5ef14bfaa344ef503707f7fa8dddfa2b1
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-10 23:49:44 +0300

    Implement public recipe list controller and added it's routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :recipes, only: [:show, :index, :new, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :foods
-
+  resources :public_recipes, only: %i[index]
   # Defines the root path route ("/")
   # root "articles#index"
 end
