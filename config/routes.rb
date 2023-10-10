e8fad0211b4604dd9b21cf751be488ea4cbc51a1
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-10 21:49:29 +0300

    Added the food routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root "foods#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :foods
 
   # Defines the root path route ("/")
   # root "articles#index"
