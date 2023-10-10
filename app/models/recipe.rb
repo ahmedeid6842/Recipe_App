bc8c0e01c42ce9ddf4c89383d4294a102fda3c2d
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-11 00:00:48 +0300

    Fixed rubocop linters errors

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
-  belongs_to :user, foreign_key: "user_id"
-  has_many :recipe_foods, foreign_key: "recipe_id"
-  has_many :foods, through: :recipe_foods, foreign_key: "food_id"
+  belongs_to :user, foreign_key: 'user_id'
+  has_many :recipe_foods, foreign_key: 'recipe_id'
+  has_many :foods, through: :recipe_foods, foreign_key: 'food_id'
 
   validates :name, :preparation_time, :cooking_time, :description, presence: true
   validates :public, inclusion: { in: [true, false] }
@@ -11,6 +11,6 @@ class Recipe < ApplicationRecord
   end
 
   def total_price
-    recipe_foods.reduce(0) { |sum, recipe_food| sum + recipe_food.quantity * recipe_food.food.price }
+    recipe_foods.reduce(0) { |sum, recipe_food| sum + (recipe_food.quantity * recipe_food.food.price) }
   end
 end
