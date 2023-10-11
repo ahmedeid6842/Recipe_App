78b32b4a8493b1ca68bdb16e754deb27753edbed
Cesar Herrera <cesarherr321@gmail.com>
2023-10-10 23:37:33 -0300

    Update recipe model for cascading destroy

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user, foreign_key: 'user_id'
-  has_many :recipe_foods, foreign_key: 'recipe_id'
+  has_many :recipe_foods, foreign_key: 'recipe_id', dependent: :destroy
   has_many :foods, through: :recipe_foods, foreign_key: 'food_id'
 
   validates :name, :preparation_time, :cooking_time, :description, presence: true
