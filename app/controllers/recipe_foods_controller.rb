70144cbfecd4e2c3dedd7327e74a00b64027c402
Cesar Herrera <cesarherr321@gmail.com>
2023-10-10 23:27:29 -0300

    Add new and create actions to RecipeFood controller

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -3,9 +3,26 @@ class RecipeFoodsController < ApplicationController
 
   def index; end
 
-  def new; end
+  def new
+    @recipe_food = RecipeFood.new
+    @recipe = Recipe.find(params[:recipe_id])
+    @foods = current_user.foods
+  end
 
-  def create; end
+  def create
+    @recipe_food = RecipeFood.new(recipe_food_params)
+    @recipe = @recipe_food.recipe
+    if @recipe_food.save
+      flash[:notice] = 'Recipe food created successfully'
+      redirect_to recipe_path(@recipe)
+    else
+      flash[:alert] = 'Error! Recipe food not created'
+      puts @recipe_food.errors.full_messages # Agregar esta línea para ver los errores de validación en la consola
+      redirect_to new_recipe_food_path(recipe_id: @recipe.id)
+    end
+  end
 
-  def destroy; end
+  def recipe_food_params
+    params.require(:recipe_food).permit(:food_id, :recipe_id, :quantity)
+  end
 end
